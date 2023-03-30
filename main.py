35d141bff9f14a8abb34a07c27cdf6ad9df2aea6
berrio25 <berriojuanpablo1@gmail.com>
2023-03-29 22:58:36 -0500

    commit

--- main.py
+++ main.py
@@ -1,34 +1,81 @@
 class Habitacion:
-    def __init__(self, num_habitacion, tipo_habitacion, capacidad, precio_noche):
-        self.num_habitacion = num_habitacion
-        self.tipo_habitacion = tipo_habitacion
-        self.capacidad = capacidad
-        self.precio_noche = precio_noche
-
-    def registrar_habitacion(self):
-        # código para almacenar la información de la habitación en la base de datos
-        pass
+    def __init__(self, numero, disponible):
+        self.numero = numero
+        self.disponible = disponible
 
 
 class Cliente:
-    def __init__(self, nombre, apellido, correo_electronico, num_telefono):
+    def __init__(self, id_cliente, nombre):
+        self.id_cliente = id_cliente
         self.nombre = nombre
-        self.apellido = apellido
-        self.correo_electronico = correo_electronico
-        self.num_telefono = num_telefono
-
-    def registrar_cliente(self):
-        # código para almacenar la información del cliente en la base de datos
-        pass
 
 
 class Pedido:
-    def __init__(self, num_habitacion, fecha_pedido, productos_solicitados, precio_total):
-        self.num_habitacion = num_habitacion
-        self.fecha_pedido = fecha_pedido
-        self.productos_solicitados = productos_solicitados
-        self.precio_total = precio_total
+    def __init__(self, id_pedido, cliente, habitacion, productos):
+        self.id_pedido = id_pedido
+        self.cliente = cliente
+        self.habitacion = habitacion
+        self.productos = productos
 
-    def registrar_pedido(self):
-        # código para almacenar la información del pedido en la base de datos
-        pass
+
+class Producto:
+    def __init__(self, id_producto, nombre, precio):
+        self.id_producto = id_producto
+        self.nombre = nombre
+        self.precio = precio
+
+
+class SistemaHotel:
+    def __init__(self):
+        self.habitaciones = []
+        self.clientes = {}
+        self.pedidos = {}
+        self.productos = {}
+        self.facturas = []
+
+    def registro_habitacion(self, numero, disponible):
+        habitacion = Habitacion(numero, disponible)
+        self.habitaciones.append(habitacion)
+
+    def registro_cliente(self, id_cliente, nombre):
+        cliente = Cliente(id_cliente, nombre)
+        self.clientes[id_cliente] = cliente
+
+    def registro_pedido(self, id_pedido, cliente, habitacion, productos):
+        pedido = Pedido(id_pedido, cliente, habitacion, productos)
+        self.pedidos[id_pedido] = pedido
+
+    def consulta_habitaciones_disponibles(self):
+        return [habitacion for habitacion in self.habitaciones if habitacion.disponible]
+
+    def registro_producto(self, id_producto, nombre, precio):
+        producto = Producto(id_producto, nombre, precio)
+        self.productos[id_producto] = producto
+
+    def consulta_precio_producto(self, id_producto):
+        return self.productos[id_producto].precio
+
+    def asignar_pedido_a_habitacion(self, id_pedido, numero_habitacion):
+        pedido = self.pedidos[id_pedido]
+        habitacion = next((h for h in self.habitaciones if h.numero == numero_habitacion), None)
+        if habitacion and habitacion.disponible:
+            pedido.habitacion = habitacion
+            habitacion.disponible = False
+        else:
+            raise ValueError("Habitación no disponible.")
+
+    def generar_factura(self, id_pedido):
+        pedido = self.pedidos[id_pedido]
+        total = sum([self.productos[p].precio for p in pedido.productos])
+        factura = {
+            'id_pedido': id_pedido,
+            'id_cliente': pedido.cliente.id_cliente,
+            'habitacion': pedido.habitacion.numero,
+            'productos': [self.productos[p] for p in pedido.productos],
+            'total': total
+        }
+        self.facturas.append(factura)
+        return factura
+
+
+
--- venv/README.md
+++ venv/README.md
@@ -1,11 +1,32 @@
-Requisitos funcionales del proyecto de servicio a la habitacion
+1. Registro de las habitaciones del hotel                           
+  1.1 Identificar las habitaciones del hotel
+2.2 Almacenar la información de los clientes en una base de datos
 
-1. Registro de las habitaciones del hotel
-1.1 Identificar las habitaciones del hotel
-2.2 Almacenar la información de los clientes en una base de datos
-2. Registro de los clientes
+2. Registro de los clientes 
 2.1 Identificar a los clientes
-2.2 Almacenar la información de los clientes en una base de datos
-3. Registro de los pedidos
+ 2.2 Almacenar la información de los clientes en una base de datos
+
+3. Registro de los pedidos 
 3.1 Identificar el pedido de la habitación
-3.2 Almacenar la información de los pedidos en una base de datos
+ 3.2 Almacenar la información de los pedidos en una base de datos	
+
+4. Consulta de habitaciones disponibles
+ 4.1 Consultar las habitaciones disponibles
+
+5. Registro de los productos del hotel 
+5.1 Identificar los productos del hotel
+ 5.2 Almacenar la información de los productos en una base de datos
+
+6. Registro de los precios de los productos
+ 6.1 Identificar el precio de los productos 
+6.2 Almacenar la información de los precios en una base de datos
+
+7. Consulta de precios de los productos  
+7.1 Consultar los precios de los productos
+
+8. Asignación de los pedidos a las habitaciones correspondientes
+ 8.1 Asignar el pedido a la habitación correspondiente
+
+9. Generación de facturas
+ 9.1 Generar la factura del pedido 
+9.2 Almacenar la información de la factura en una base de datos
